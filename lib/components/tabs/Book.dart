b247e4e7482965bee15906c7562f19a94d4fcf18
supeng123 <492526873@qq.com>
2019-07-06 20:58:44 +0800

    search field on tabbar

--- lib/components/tabs/Book.dart
+++ lib/components/tabs/Book.dart
@@ -7,6 +7,10 @@ class BookPage extends StatefulWidget {
 
 class _BookPageState extends State<BookPage> with SingleTickerProviderStateMixin {
   TabController contorller;
+
+  Widget appBarTitle = new Text("Search Example", style: new TextStyle(color: Colors.white));
+  Icon icon = new Icon(Icons.search, color: Colors.white);
+
   @override
   void initState() {
     super.initState();
@@ -17,13 +21,57 @@ class _BookPageState extends State<BookPage> with SingleTickerProviderStateMixin
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
+        title: appBarTitle,
+        centerTitle: true,
+        actions:<Widget>[
+          IconButton(
+            icon: icon,
+            onPressed: (){
+              setState(() {
+                if(this.icon.icon == Icons.search) {
+                  this.icon = new Icon(
+                    Icons.close,
+                    color: Colors.black,
+                  );
+                  this.appBarTitle = new TextField(
+                    style: new TextStyle(
+                      color: Colors.black
+                    ),
+                    decoration: new InputDecoration(
+                      prefixIcon: new Icon(Icons.search, color: Colors.black),
+                      hintText: "Search...",
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintStyle: new TextStyle(color: Colors.black),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(5.0)))
+                    ),
+                    onChanged: (text){
+                      print(text);
+                    },
+                  );
+                } else {
+                  setState(() {
+                    this.icon = new Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    );
+                    this.appBarTitle = new Text(
+                      "Search Sample",
+                      style: new TextStyle(color: Colors.white),
+                    );
+                  });
+                }
+              });
+            },
+          )
+        ],
         bottom: TabBar(
           controller: contorller,
           tabs: <Tab>[
-            Tab(text: "hot topic"),
-            Tab(text: "recommend"),
+            Tab(text: "book"),
             Tab(text: "movie"),
-            Tab(text: "music")
+            Tab(text: "music"),
+            Tab(text: "market")
           ]
         ),
         
